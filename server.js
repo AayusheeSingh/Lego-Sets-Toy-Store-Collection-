90125ac39756d5da2208abf97331b4643acad8e6
Aayushee Singh <aayusheesingh19@gmail.com>
2023-12-11 23:14:27 -0500

    Add all files

--- server.js
+++ server.js
@@ -1,31 +1,34 @@
-/**************************************************************************************************************
-* WEB322 – Assignment 05
+/********************************************************************************
+*  WEB322 – Assignment 06
 * 
-* I declare that this assignment is my own work in accordance with Seneca's
-* Academic Integrity Policy:
+*  I declare that this assignment is my own work in accordance with Seneca's
+*  Academic Integrity Policy:
 * 
-* https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
+*  https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
 * 
-* Name: Aayushee Singh        Student ID:   173927211       Date: 27-11-2023
+*  Name: _________Aayushee Singh_____________ Student ID: ____173927211__________ Date: _____11-12-2023_________
 *
-* Published URL: https://eagle-suspenders.cyclic.app/
-**************************************************************************************************************/
+*  Published URL: __________https://eagle-suspenders.cyclic.app/_________________________________________________
+*
+********************************************************************************/
+
 
 const express = require('express');
 const path = require('path');
 const legoData = require('./modules/legoSets');
+const authData = require('./modules/auth-service');
+const clientSessions = require('client-sessions');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-/***********************************  Middleware *************************************/
+/*********************************** Middleware *************************************/
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 
-
-/*************************** Middleware for error handling   ***********************/
+/*************************** Middleware for error handling ***********************/
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).render('500', { message: 'Something went wrong on the server', page: req.url });
@@ -35,6 +38,8 @@ app.use((err, req, res, next) => {
 app.use(async (req, res, next) => {
   try {
     await legoData.initialize(app);
+    await authData.initialize();
+
     next();
   } catch (error) {
     console.error('Initialization error:', error);
@@ -42,11 +47,36 @@ app.use(async (req, res, next) => {
   }
 });
 
+/************************** Client Session Middleware **************************/
+app.use(
+  clientSessions({
+    cookieName: 'session',
+    secret: 'o6LjQ5EVNC28ZgK64hDELM18ScpFQr',
+    duration: 2 * 60 * 1000,
+    activeDuration: 1000 * 60,
+  })
+);
+
+/************************* Middleware for Session Access ************************/
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
+
 /********************* Navbar item for Add Set ********************************/
 app.use((req, res, next) => {
   res.locals.page = req.path;
   next();
 });
+
+/*********************** Helper Middleware Function ***************************/
+const ensureLogin = (req, res, next) => {
+  if (!req.session || !req.session.user) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
 
 // Routes
 app.get('/', (req, res) => res.render('home'));
@@ -74,7 +104,7 @@ app.get('/lego/sets/:set_num', async (req, res) => {
 });
 
 // Add set route
-app.get('/lego/addSet', async (req, res) => {
+app.get('/lego/addSet', ensureLogin, async (req, res) => {
   try {
     const themes = await legoData.getAllThemes();
     res.render('addSet', { themes });
@@ -83,7 +113,7 @@ app.get('/lego/addSet', async (req, res) => {
   }
 });
 
-app.post('/lego/addSet', async (req, res) => {
+app.post('/lego/addSet', ensureLogin, async (req, res) => {
   try {
     const setData = {
       set_num: req.body.set_num,
@@ -101,7 +131,7 @@ app.post('/lego/addSet', async (req, res) => {
 });
 
 // Edit set route
-app.get('/lego/editSet/:set_num', async (req, res) => {
+app.get('/lego/editSet/:set_num', ensureLogin, async (req, res) => {
   try {
     const setNum = req.params.set_num;
     const [set, themes] = await Promise.all([
@@ -115,7 +145,7 @@ app.get('/lego/editSet/:set_num', async (req, res) => {
   }
 });
 
-app.post('/lego/editSet', async (req, res) => {
+app.post('/lego/editSet', ensureLogin, async (req, res) => {
   try {
     const setNum = req.body.set_num;
     const setData = {
@@ -133,7 +163,7 @@ app.post('/lego/editSet', async (req, res) => {
   }
 });
 
-app.get('/lego/deleteSet/:set_num', async (req, res) => {
+app.get('/lego/deleteSet/:set_num', ensureLogin, async (req, res) => {
   try {
     const setNum = req.params.set_num;
     await legoData.deleteSet(setNum);
@@ -144,11 +174,60 @@ app.get('/lego/deleteSet/:set_num', async (req, res) => {
 });
 
 
+app.get('/login', (req, res) => {
+  res.render('login', { errorMessage: null, userName: '' });
+});
+
+app.get('/register', (req, res) => {
+  res.render('register', { successMessage: null, errorMessage: null, userName: '' });
+});
+
+
+app.post('/register', async (req, res) => {
+  try {
+    const userData = req.body;
+    await authData.registerUser(userData);
+    res.render('register', { successMessage: 'User created' });
+  } catch (err) {
+    res.render('register', { errorMessage: err, userName: req.body.userName, successMessage: null });
+  }
+});
+
+
+app.post('/login', async (req, res) => {
+  try {
+    req.body.userAgent = req.get('User-Agent');
+    const user = await authData.checkUser(req.body);
+    req.session.user = {
+      userName: user.userName,
+      email: user.email,
+      loginHistory: user.loginHistory,
+    };
+    console.log('Session User:', req.session.user); // Add this line
+    res.redirect('/lego/sets');
+  } catch (err) {
+    res.render('login', { errorMessage: err, userName: req.body.userName });
+  }
+});
+
+
+// GET route for logout
+app.get('/logout', (req, res) => {
+  req.session.reset();
+  res.redirect('/');
+});
+
+
+app.get('/userHistory', ensureLogin, (req, res) => {
+  res.render('userHistory', { session: req.session }); 
+});
+
+
+
 // 404 route
 app.use((req, res, next) => {
   res.status(404).render('404', { message: "I'm sorry, we're unable to find what you're looking for" });
 });
-
 
 // Start the server
 app.listen(PORT, () => {
